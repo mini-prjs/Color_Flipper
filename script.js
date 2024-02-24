83ca1a4bb962edc82dfeab763c716ddf8e1d2ac6
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-02-24 15:16:10 +0600

    final

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const btn = document.getElementById('btn');
-const body = document.getElementsByName('body');
+const btn = document.getElementById("btn");
+const body = document.getElementsByName("body");
+const color = document.getElementById("color");
 
 const change = () => {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = "#" + randomColor;
-    color.innerHTML = "#" + randomColor;
-    console.log(randomColor);
-}
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = "#" + randomColor;
+  color.innerHTML = "#" + randomColor;
+  hex.innerHTML = "#" + randomColor;
+};
 
-btn.addEventListener('click', change);
+btn.addEventListener("click", change);
